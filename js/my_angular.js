94d848a5b99cd96c43a57dd5adc2422337104a55
Tyler Bowers <tyler.j.bowers@gmail.com>
2017-11-01 19:07:05 -0600

    updated angular

--- js/my_angular.js
+++ js/my_angular.js
@@ -1,8 +1,13 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('JokeMaker', function($scope) {
+  $scope.joke_text = 'Why did the chicken cross the road?';
   $scope.answer = 'To get to the other side';
-  
+
+  var newJoke = "What's black and white?"
+  var newAnswer = "A newspaper"
+
+
   $scope.updatePunchline = function() {
     console.log("updatePunchline called");
     $scope.punchline = $scope.answer.toUpperCase() + '!';
@@ -11,6 +16,7 @@ myApp.controller('JokeMaker', function($scope) {
   $scope.resetJoke = function() {
     console.log("resetJoke called");
     $scope.punchline = '';
+    $scope.joke_text = newJoke;
   };
 
 });
